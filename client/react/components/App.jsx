59c943c4f4e783ebe6ad63bad90b503f77af2049
Justinlkirk <justinleekirk@gmail.com>
2022-01-29 16:13:29 -0600

    Got the dev buttons set up

--- client/react/components/App.jsx
+++ client/react/components/App.jsx
@@ -3,13 +3,17 @@ import { connect } from 'react-redux';
 import * as actions from '../../redux/actions/actions.js';
 import SignUp from './SignUp.jsx';
 import Login from './Login.jsx';
+import CommunityContainer from '../containers/CommunityContainer.jsx';
+import ProfileContainer from '../containers/ProfileContainer.jsx';
 
 const mapStateToProps = (state) => ( { page: state.page } );
 
 
 const mapDispatchToProps = dispatch => ( {
   changeToLoginPageActionCreator: () => dispatch(actions.changeToLoginPageActionCreator()),
-  changeToSignUpPageActionCreator: () => dispatch(actions.changeToSignUpPageActionCreator())
+  changeToSignUpPageActionCreator: () => dispatch(actions.changeToSignUpPageActionCreator()),
+  changeToCommunityPageActionCreator: () => dispatch(actions.changeToCommunityPageActionCreator()),
+  changeToProfilePageActionCreator: () => dispatch(actions.changeToProfilePageActionCreator())
 } );
 
 
@@ -22,18 +26,23 @@ class App extends Component {
   render() {
     const display = [<h1 key='title'>brdl</h1>]
     if (this.props.page === 'signUp') {
-      display.push(<SignUp />)
+      display.push(<SignUp key='su' />)
       // display.push(<h2 key='signUp'>Sign Up</h2>);
       // display.push(<button key='lB' onClick={this.props.changeToLoginPageActionCreator}>Have an account?</button>)
-    }
-    else if (this.props.page === 'login') {
+    } else if (this.props.page === 'login') {
       display.push(<Login />)
       // display.push(<h2 key='login'>Login</h2>);
       // display.push(<button key='sB' onClick={this.props.changeToSignUpPageActionCreator}>Need an account?</button>)
-    }
+    } else if (this.props.page === 'community') display.push(<CommunityContainer />);
+    else if (this.props.page === 'profile') display.push(<ProfileContainer />);
+
     return (
       <div>
         {display}
+        <div className='dev'>
+          <button key='cB' onClick={this.props.changeToCommunityPageActionCreator}>Dev jump to community page</button>
+          <button key='pB' onClick={this.props.changeToProfilePageActionCreator}>Dev jump to profile page</button>
+        </div>
       </div>
     )
   }
--- client/redux/constants/actionTypes.js
+++ client/redux/constants/actionTypes.js
@@ -1,2 +1,4 @@
 export const LOGIN = 'LOGIN';
 export const SIGN_UP = 'SIGN_UP';
+export const COMMUNITY = 'COMMUNITY';
+export const PROFILE = 'PROFILE';
