db13460c7e13af2b7743b6e595bb25e17d89945c
Justinlkirk <justinleekirk@gmail.com>
2022-01-28 23:07:40 -0600

    yessssssssssssssssssssssssssssssssssssssssssssssss

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,53 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: './client/index.js',
-  output: { path: './build' },
-  devServer: { static: { directory: '/home/kirk/scratch-project/client' } },
-  plugins: new HtmlWebpackPlugin({ template: './index.html' }),
+  mode: 'development',
+  entry: path.join(__dirname, './client/index.js'),
+  output:{
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.join(__dirname, 'index.html')})
+  ],
   module: {
-    rules: [ {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: { 
-        loader: 'babel-loader',
-        options: { presets: ['@babel/preset-env', '@babel/preset-react'] }
-      }},
-    {
-      test: /\.s?css$/i,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    },
-    {
-      test: /\.(png|jp(e*)g|svg|gif)$/,
-      use: ['file-loader'],
-    }
-  ]
+    rules: [
+        {
+        test: /\.?jsx?/,
+        exclude: /node_modules/,
+        use: [{
+            loader: "babel-loader",
+            options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+             }
+          }]
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+    ]
+  },
+  devServer: {
+    static: {
+            directory: path.resolve(__dirname, './build'),
+            publicPath: path.resolve(__dirname, './build'),
+          },
+      },
   }
-}
+
+
+// devServer: {
+//   static: {
+//     directory: path.resolve(__dirname, './build'),
+//     publicPath: path.resolve(__dirname, './build'),
+//   },
+//   proxy: { '/querydb': 'http://localhost:3000' },
+// },
+
+// devServer: {
+//   proxy: {
+//     '/*': {
+//       target: 'http://localhost:3000'
+//     }
+//   }
+// }
