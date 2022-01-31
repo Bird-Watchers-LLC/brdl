5ca9e31c927c02bbd518e2799abffeabd54fab6e
Julia Bakerink <juliabakerink@Julias-MacBook-Pro.local>
2022-01-31 14:36:54 -0800

    finished adding birds to DB when GET req -Calvin/Julia

--- server/controllers/birdController.js
+++ server/controllers/birdController.js
@@ -11,18 +11,36 @@ birdController.nearby = async (req, res, next) => {
     const { username, lat, long } = req.query;
          // lat/long must be up to 2 decimal points
     try {
-        const apiResponse = await axios.get(`https://api.ebird.org/v2/data/obs/geo/recent?lat=${lat}&lng=${long}&maxResults=10`, {
+        const apiResponse = await axios.get(`https://api.ebird.org/v2/data/obs/geo/recent?lat=${lat}&lng=${long}&maxResults=5`, {
             headers: { "x-ebirdapitoken": "er9pqjjuc7rv" },
            })
-        // filter data by comName, sciName, location name
         const newBirdList = apiResponse.data.map(bird => ({
             sciName: bird.sciName,
             comName: bird.comName,
             locName: bird.locName
         }))
-        res.locals.nearby = { birds: newBirdList }; 
-        
-        // may need to insert 10 birds into db here, but will need to ensure scientific_name type is set to UNIQUE
+        res.locals.nearby = { birds: newBirdList };
+        // INSERTING ONE BY ONE
+        const queryString = `INSERT INTO Birds (scientific_name, common_name) VALUES ($1, $2) ON CONFLICT (scientific_name) DO NOTHING`;
+        newBirdList.forEach(async function (bird) {
+            try { await db.query(queryString, [bird.sciName, bird.comName]) }
+            catch (err) {
+                console.log('Cannot insert bird into Birds table');
+                console.log(err);
+            }
+        })
+
+        // INSERTING ALL AT ONCE IGNORE DUPLICATES (error- if bird name contains single quote, need to add escape character to ignore)
+        // const queryStart = 'INSERT INTO Birds (scientific_name, common_name) VALUES ';
+        // const queryMid = newBirdList.map(bird => {
+        //     return `("${bird.sciName}", "${bird.comName}")`
+        // }).join(', ');
+        // const queryEnd = ' ON CONFLICT (scientific_name) DO NOTHING';
+        // const queryString = queryStart + queryMid + queryEnd;
+        // db.query(queryString)
+        // .then(() => console.log('successfully added'));
+
+
         return next();
     } catch (err) {
         return next({
@@ -38,24 +56,38 @@ birdController.nearby = async (req, res, next) => {
 // we will respond with T/F if bird was successfully added to database
 // birdController.seen = async (req, res, next) => {
 //     try {
-//         const { username, lat, long, timeStamp, sciBirdName } = req.query;
-//         const queryString = `INSERT INTO seen_birds (username, scientific_name, time_stamp) VALUES ($1, $2, $3)`
-//         const queryResult = await db.query(queryString, [username, sciBirdName, timeStamp]);
-//         console.log(queryResult);
-//         // need to set conditional
-//         // hitting an issue when testing where current_timestamp is "no longer supported"
-//         // res.locals.seen = {valid: false};
-//         // res.locals.seen = {valid: true};
-//         return next()
-//     } catch (err) {
-//         return next({
-//             log: `Express error handler caught in birdController.seen: ${err.message}`,
-//             status: 500,
-//             message: { err: 'Express error handler caught in birdController.seen' }
-//         });
+        // const { username, lat, long, timeStamp, sciBirdName, comBirdName } = req.query;
+        // check if the bird exists in birds table, if it does, move into next step; if it doesn't insert into birds table
+        // const queryString = "SELECT * FROM Birds WHERE scientific_name=$1"
+        // const queryResult = await db.query(queryString, [sciBirdName]);
+        // console.log(queryResult);
+        // if (!queryResult.rows.length) {
+        // const queryString = `INSERT INTO Birds (scientific_name, ) VALUES ($1, $2, $3)`
+        // const queryResult = await db.query(queryString, [username, sciBirdName, timeStamp]);
+        // }
+        // create an entry into the seen birds table where we provide username, sciname, and timestamp
+        // const queryString = `INSERT INTO seen_birds (username, scientific_name, time_stamp) VALUES ($1, $2, $3)`
+        // const queryResult = await db.query(queryString, [username, sciBirdName, timeStamp]);
+        // console.log(queryResult);
+        // need to set conditional
+        // hitting an issue when testing where current_timestamp is "no longer supported"
+        // res.locals.seen = {valid: false};
+        // res.locals.seen = {valid: true};
+        // return next()
+    // } catch (err) {
+    //     return next({
+    //         log: `Express error handler caught in birdController.seen: ${err.message}`,
+    //         status: 500,
+    //         message: { err: 'Express error handler caught in birdController.seen' }
+    //     });
 
-//     }
+    // }
 // };
+
+//   const queryString = `INSERT INTO Birds (scientific_name, common_name) VALUES ($1, $2)`
+// const queryResult = await db.query(queryString, [newBirdList.sciName,  ]);
+// may need to insert 10 birds into db here, but will need to ensure scientific_name type is set to UNIQUE
+// 
 
 // DELETE -- when a user "unclicks" a bird they have seen, client will provide username, sciBirdName
 // we will respond with T/F if bird was successfully deleted from database
