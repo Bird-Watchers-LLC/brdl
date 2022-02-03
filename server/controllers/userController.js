b0437b033926b0ea8aa850c834201345c70a6c1a
Eric Gomez <ergomez0201@gmail.com>
2022-02-02 20:04:42 -0800

    added link to postgresql in brdModels file, connected signup-gainAccess route to the db

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,11 +15,10 @@ userController.auth = async (req, res, next) => {
       console.log(`Auth failed using username: ${clientUsername} and password: ${clientPassword}`);
       res.locals.auth = { valid: false };
       return next();
-    } else {
-      res.locals.auth = { valid: true, fullName: queryResult.rows[0].name };
-      console.log(`Auth success using username: ${clientUsername} and password: ${clientPassword}`);
-      return next();
     }
+    res.locals.auth = { valid: true, fullName: queryResult.rows[0].name };
+    console.log(`Auth success using username: ${clientUsername} and password: ${clientPassword}`);
+    return next();
   } catch (err) {
     return next({
       log: `Express error handler caught in userController.auth: ${err.message}`,
@@ -29,25 +28,29 @@ userController.auth = async (req, res, next) => {
   }
 };
 
-// client will provide a unique username and a password in req.query
+// client will provide a unique username and a password in req.body
 // we will query database with both the username and password. If the db does not contain username, it will store the username and password and set res.locals.auth = true
 // else set res.locals.auth to false if username already exists
 userController.create = async (req, res, next) => {
+  // destructure username,password, and fullname from req.body
+  // note - double check with post request on variable names
+  console.log(req.body);
+  const { username, password, fullname } = req.body;
   try {
-    const queryCheckString = 'SELECT * FROM Users WHERE username=$1';
-    const queryCheckResult = await db.query(queryCheckString, [clientUsername]);
+    const queryCheckString = `SELECT * FROM users WHERE username = $1`;
+    const queryCheckResult = await db.query(queryCheckString, [username]);
+    // console.log(queryCheckResult);
     if (queryCheckResult.rows.length > 0) {
       res.locals.auth = { valid: false };
       console.log('username already exist');
       return next();
-    } else {
-      const queryString = 'INSERT INTO Users (name, username, password) VALUES ($1, $2, $3)';
-      const queryResult = await db.query(queryString, [fullName, clientUsername, clientPassword]);
-      res.locals.auth = { valid: true };
-      console.log('account succcessfully made');
-      console.log(queryResult);
-      return next();
     }
+    const queryString = `INSERT INTO users (name, username, password) VALUES ($1, $2, $3) RETURNING *`;
+    const queryResult = await db.query(queryString, [fullname, username, password]);
+    res.locals.auth = { valid: true, fullname, username };
+    console.log('account succcessfully made');
+    console.log(queryResult);
+    return next();
   } catch (err) {
     return next({
       log: `Express error handler caught in userController.create: ${err.message}`,
